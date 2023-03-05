c4c4125b8573ce3e6abcc2878a7978046161dfa8
s13bp <86206902+s13bp@users.noreply.github.com>
2023-03-05 21:26:23 +0300

    Add files via upload

---
+++ precache-manifest.045b3144ec20df3833354f0490136326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0463daee7f6ae030c416bf7a4cc9d923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "589400bef8e380210c40",
+    "url": "/static/css/main.6e99b7f0.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589400bef8e380210c40",
+    "url": "/static/js/main.80f20f05.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
